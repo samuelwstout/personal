56f14c4f397c7fafb61e936b25664ae10403a4df
samuelwstout <samuelwstout@gmail.com>
2022-03-18 18:10:14 -0500

    Organize projects to divs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,47 +9,62 @@ const Projects = () => {
          <Button className='homeButton' variant='outlined'>
             <Link className='homeButtonText' to='/'>Home</Link>
         </Button>
+        <iframe width="300" height="200" className='itunesvid'
+                    src="https://youtube.com/embed/2dPR0WBXx2w"   
+                    allowfullscreen="allowfullscreen"
+                    mozallowfullscreen="mozallowfullscreen" 
+                    msallowfullscreen="msallowfullscreen" 
+                    oallowfullscreen="oallowfullscreen" 
+                    webkitallowfullscreen="webkitallowfullscreen">
+        </iframe>
+        <iframe width="300" height="200" className='pizzavid'
+                    src="https://youtube.com/embed/oFFeVwqrzQ4"  
+                    allowfullscreen="allowfullscreen"
+                    mozallowfullscreen="mozallowfullscreen" 
+                    msallowfullscreen="msallowfullscreen" 
+                    oallowfullscreen="oallowfullscreen" 
+                    webkitallowfullscreen="webkitallowfullscreen">
+        </iframe>
         <div className="projectsdiv">
-        <div className='iTunes'>
-            <h1>iTunes Search</h1>
-            <iframe width="500" height="400" className='itunesvid'
-                src="https://youtube.com/embed/2dPR0WBXx2w"   
-                allowfullscreen="allowfullscreen"
-                mozallowfullscreen="mozallowfullscreen" 
-                msallowfullscreen="msallowfullscreen" 
-                oallowfullscreen="oallowfullscreen" 
-                webkitallowfullscreen="webkitallowfullscreen">
-            </iframe>
-            <p><a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://itunes-search-83cf6.firebaseapp.com/'>Go to Site</a></p>
-            <p><a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://github.com/samuelwstout/music-search-app'>link to GitHub</a></p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>iTunes Search API</li>
-            </ul>
+            <div className='iTunes'>
+                <h1>iTunes Search</h1>
+                <p>
+                   Search application that returns results from iTunes.
+                </p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>iTunes Search API</li>
+                </ul>
+            <Button className='homeButton' variant='outlined'>
+                <a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://itunes-search-83cf6.firebaseapp.com/'>Live Demo</a>
+            </Button>
+            <Button className='homeButton' variant='outlined'>
+                <a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://github.com/samuelwstout/music-search-app'>Github Repo</a>
+            </Button>
+            </div>
+
+            <div className='deepdish'>
+                <h1>Discover Deep Dish</h1>
+                <p>
+                    Restaurant-finding application exclusively for deep dish pizza.
+                </p>
+                <ul>
+                    <li>ReactJS</li>
+                    <li>React Router</li>
+                    <li>Material UI</li>
+                    <li>Mapbox Gl JS API</li>
+                    <li>json-server (mock server)</li>
+                </ul>
+            <Button variant='outlined'>
+                <a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://uw87tu99.web.app/'>Live Demo</a>
+            </Button>
+            <Button variant='outlined'>
+                <a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://github.com/samuelwstout/phase-2-final-project'>Github Repo</a>
+            </Button>
+            </div>
         </div>
-        <div className='deepdish'>
-            <h1>Discover Deep Dish</h1>
-            <iframe width="420" height="315"
-                src="https://youtube.com/embed/oFFeVwqrzQ4"  
-                allowfullscreen="allowfullscreen"
-                mozallowfullscreen="mozallowfullscreen" 
-                msallowfullscreen="msallowfullscreen" 
-                oallowfullscreen="oallowfullscreen" 
-                webkitallowfullscreen="webkitallowfullscreen">
-            </iframe>
-            <p><a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://uw87tu99.web.app/'>Go to Site</a></p>
-            <p><a className='githubLink' target="_blank" rel="noopener noreferrer" href='https://github.com/samuelwstout/phase-2-final-project'>link to GitHub</a></p>
-            <ul>
-                <li>ReactJS</li>
-                <li>Material UI</li>
-                <li>Mapbox Gl JS API</li>
-            </ul>
-        </div>
-        <div>
-        </div>
-    </div>
     </div>
     )
 }

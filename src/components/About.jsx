8312d8a3822cd153a174942d1d49c04cdfe684a2
samuelwstout <samuelwstout@gmail.com>
2022-05-31 13:32:59 -0500

    Start README.md

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
                 <Link className='buttonText' to='/'>Back</Link>
       </Button>
       <div className="aboutDiv">
-      <p className='about1'>Frontend focused web developer with experience building web applications in JavaScript (React), HTML, and CSS. I have strong experience integrating RESTful APIs and incorporating authentication and authorization with NodeJS (Express). I'm currently enrolled in Flatiron School's software engineering program, learning Ruby to further grow my skills into backend web development.</p>
+      <p className='about1'>Frontend focused web developer with experience building web applications in JavaScript (React), HTML, and CSS. I have experience integrating RESTful APIs and incorporating authentication and authorization with NodeJS (Express). I'm currently enrolled in Flatiron School's software engineering program, learning Ruby to further grow my skills into backend web development.</p>
       
       <p className='about2'>Previously, I was a student and semi-professional orchestral trumpet player. I studied at the Peabody Institute of the Johns Hopkins University and held a substitute position at the New World Symphony. Prior to attending Peabody, I toured as a member of the National Youth Orchestra of the United States (NYO-USA) under the direction of Michael Tilson Thomas. I have been actively studying and practicing music for 14 years.</p>
 
